7fd921226bd500bc8149a231cf7b17607680dfaf
LorenaOviedo <loreovie@gmail.com>
2026-03-01 03:52:38 -0600

    Se integra auth.php

--- views/tratamientos.php
+++ views/tratamientos.php
@@ -3,6 +3,7 @@
 require_once __DIR__ . '/../config/config.php';
 require_once __DIR__ . '/../config/database.php';
 require_once __DIR__ . '/../controllers/AuthController.php';
+require_once __DIR__ . '/../config/auth.php';
 
 session_start();
 
