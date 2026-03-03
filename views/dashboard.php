b22d7af044c1f5f468ad39e0e0b975333169a061
LorenaOviedo <loreovie@gmail.com>
2026-03-03 01:25:51 -0600

    Debug inicio

--- views/dashboard.php
+++ views/dashboard.php
@@ -3,8 +3,13 @@
 require_once __DIR__ . '/../config/config.php';
 require_once __DIR__ . '/../config/database.php';
 require_once __DIR__ . '/../controllers/AuthController.php';
-require_once __DIR__ . '/../config/auth.php';
 
+// DEBUG TEMPORAL
+session_start();
+var_dump($_SESSION);
+die();
+
+require_once __DIR__ . '/../config/auth.php';
 
 session_start();
 
