d2fa6ebf09af8690754998e20ab8cb82451b236f
LorenaOviedo <loreovie@gmail.com>
2026-03-11 01:41:16 -0600

    Correcion de responsive

--- includes/header.php
+++ includes/header.php
@@ -1,3 +1,7 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
 <?php
 /*Header del sistema de gestión integral Maxilofacial Texcoco*/
 // Enlace configuración
