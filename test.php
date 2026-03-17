fce3167cc3face931fdefea86d1b81bb5c6f9980
LorenaOviedo <loreovie@gmail.com>
2026-03-17 13:35:36 -0600

    Debug login test 2

--- test.php
+++ test.php
@@ -1,2 +1,8 @@
 <?php
-echo "PHP funciona";
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+require_once __DIR__ . '/config/config.php';
+require_once __DIR__ . '/config/database.php';
+
+echo "Conexión OK";
