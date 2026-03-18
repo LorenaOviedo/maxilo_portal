7312adf9a665a76851c0443975e07e9b27be5e06
LorenaOviedo <loreovie@gmail.com>
2026-03-17 23:28:17 -0600

    Correccion procedimientos nombre modelo

--- views/procedimientos.php
+++ views/procedimientos.php
@@ -3,7 +3,7 @@ require_once __DIR__ . '/../config/config.php';
 require_once __DIR__ . '/../config/database.php';
 require_once __DIR__ . '/../controllers/AuthController.php';
 require_once __DIR__ . '/../config/auth.php';
-require_once __DIR__ . '/../models/Procedimiento.php';
+require_once __DIR__ . '/../models/Procedimientos.php';
  
 session_start();
  
