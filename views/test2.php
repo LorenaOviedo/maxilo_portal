80256a3a1ed931afc01ae87e9aff14207cd73940
LorenaOviedo <loreovie@gmail.com>
2026-03-26 15:07:06 -0600

    Debug test 3

--- views/test2.php
+++ views/test2.php
@@ -2,32 +2,28 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-echo "1. config<br>";
 require_once __DIR__ . '/../config/config.php';
-
-echo "2. database<br>";
 require_once __DIR__ . '/../config/database.php';
-
-echo "3. AuthController<br>";
 require_once __DIR__ . '/../controllers/AuthController.php';
-
-echo "4. auth.php<br>";
 require_once __DIR__ . '/../config/auth.php';
+require_once __DIR__ . '/../models/Dashboard.php';
 
-echo "5. session_start<br>";
 session_start();
 
-echo "6. AuthController instancia<br>";
-$auth = new AuthController();
+$page_title = 'Dashboard';
+$db         = getDB();
+$dashboard  = new Dashboard($db);
+$datos      = $dashboard->resumen();
 
-echo "7. verificarSesion<br>";
-var_dump($auth->verificarSesion());
+echo "datos OK<br>";
 
-echo "8. header.php existe: ";
-var_dump(file_exists(__DIR__ . '/../includes/header.php'));
+echo "11. incluyendo header<br>";
+include __DIR__ . '/../includes/header.php';
 
-echo "9. sidebar.php existe: ";
-var_dump(file_exists(__DIR__ . '/../includes/sidebar.php'));
+echo "12. incluyendo sidebar<br>";
+include __DIR__ . '/../includes/sidebar.php';
 
-echo "10. footer.php existe: ";
-var_dump(file_exists(__DIR__ . '/../includes/footer.php'));
+echo "13. incluyendo footer<br>";
+include __DIR__ . '/../includes/footer.php';
+
+echo "Todo OK";
