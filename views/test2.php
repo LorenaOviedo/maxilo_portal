c405040d5d3ff1117b703808a13e70aedfb717c0
LorenaOviedo <loreovie@gmail.com>
2026-03-26 15:41:28 -0600

    Debug db

--- views/test2.php
+++ views/test2.php
@@ -1,19 +1,3 @@
 <?php
-require_once __DIR__ . '/../config/config.php';
-require_once __DIR__ . '/../config/database.php';
-require_once __DIR__ . '/../models/Dashboard.php';
-
-$db   = getDB();
-$dash = new Dashboard($db);
-
-try {
-    echo "resumen...<br>";
-    $datos = $dash->resumen();
-    echo "<pre>";
-    print_r($datos);
-    echo "</pre>";
-} catch (Exception $e) {
-    echo "<b>ERROR:</b> " . $e->getMessage();
-} catch (Error $e) {
-    echo "<b>FATAL:</b> " . $e->getMessage();
-}
+$datos = $dash->resumen();
+print_r($datos);
