501379a8d152e9797e9af90b9cfa4aaf53f61f10
LorenaOviedo <loreovie@gmail.com>
2026-03-26 15:02:49 -0600

    Debug rutas

--- test.php
+++ test.php
@@ -1,16 +1,13 @@
 <?php
-// Muestra la estructura real de carpetas
-$base = '/home/u113135450/domains/portal-maxilofacial.site/';
-foreach (new DirectoryIterator($base) as $item) {
-    if ($item->isDot()) continue;
-    echo ($item->isDir() ? '[DIR] ' : '[FILE] ') . $item->getFilename() . '<br>';
-}
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
 
-echo '<hr>';
+echo "views está en: " . __DIR__ . "<br>";
+echo "¿config existe?: ";
+var_dump(is_dir(__DIR__ . '/../config'));
 
-// También muestra qué hay dentro de public_html
-echo '<b>Dentro de public_html:</b><br>';
-foreach (new DirectoryIterator($base . 'public_html/') as $item) {
-    if ($item->isDot()) continue;
-    echo ($item->isDir() ? '[DIR] ' : '[FILE] ') . $item->getFilename() . '<br>';
-}
+echo "¿models existe?: ";
+var_dump(is_dir(__DIR__ . '/../models'));
+
+echo "¿controllers existe?: ";
+var_dump(is_dir(__DIR__ . '/../controllers'));
