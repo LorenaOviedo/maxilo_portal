ed52c9dcc1dce260f2ffb6c1f0f17e65df7e092d
LorenaOviedo <loreovie@gmail.com>
2026-03-28 04:35:14 -0600

    Correcion de día db

--- config/database.php
+++ config/database.php
@@ -63,7 +63,7 @@ class Database
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
                 PDO::ATTR_EMULATE_PREPARES => false,
-                PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES {$this->charset}"
+                PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES {$this->charset}, time_zone = '-06:00'"
             ];
             
             $this->connection = new PDO($dsn, $this->username, $this->password, $options);
