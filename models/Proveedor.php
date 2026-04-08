2c09f2c002f3c36f83e3fb4a9ea45b9ba0b5537d
LorenaOviedo <loreovie@gmail.com>
2026-04-08 02:45:03 -0600

    Correcion producto servicio

--- models/Proveedor.php
+++ models/Proveedor.php
@@ -153,7 +153,7 @@ class Proveedor
     public function getCatalogos(): array
     {
         return [
-            'tiposproductoproveedor' => $this->_query(
+            'tipoproductoproveedor' => $this->_query(
                 "SELECT id_tipo_producto_proveedor, tipo_producto_proveedor
                  FROM tipoproductoproveedor ORDER BY tipo_producto_proveedor"
             ),
