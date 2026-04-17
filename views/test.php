189eff75cd8255de2305c0f527481e3489a827cb
LorenaOviedo <loreovie@gmail.com>
2026-04-17 00:10:04 -0600

    Debug info

--- views/test.php
+++ views/test.php
@@ -1,7 +1,3 @@
 <?php
-require_once __DIR__ . '/../vendor/phpmailer/Exception.php';
-echo 'Exception OK';
-require_once __DIR__ . '/../vendor/phpmailer/SMTP.php';
-echo 'SMTP OK';
-require_once __DIR__ . '/../vendor/phpmailer/PHPMailer.php';
-echo 'PHPMailer OK';
+phpinfo();
+?>
